13e9612862c4a53575be3bcf503d704d6286641b
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-10 19:09:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
 
+document.getElementById('addTodoBtn').addEventListener('click', function() {
+ let inputValue = document.getElementById('newTodoInput').value;
+
+ if (inputValue.trim() !== '') {
+    let li = document.createElement('li');
+    li.textContent = inputValue;
+    document.getElementById('todoList').appendChild(li);
+    document.getElementById('newTodoInput').value = '';
+ }
+});
